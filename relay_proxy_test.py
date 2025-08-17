ffcc05ff76d5d2eecab1f2256481b75e359066cc
Marshall Yount <myount@rippling.com>
2025-08-17 06:31:34 -0700

    local test script for relay proxy

---
+++ relay_proxy_test.py
@@ -0,0 +1,41 @@
+# /// script
+# dependencies = [
+#   "launchdarkly-server-sdk==9.11.1",
+# ]
+# ///
+import time, logging
+from ldclient import Context, LDClient
+from ldclient.config import Config
+import os
+
+RELAY_PROXY_URI = "http://localhost:8030"
+
+sdkKey = os.getenv("LD_ENV_DEVELOPMENT_SDK_KEY")
+logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(levelname)s:%(name)s:%(message)s")
+
+
+def get_client():
+    config = Config(
+        sdkKey,
+        application={"id": "failure-mode-test"},
+        initial_reconnect_delay=1,
+        poll_interval=30,
+        base_uri=RELAY_PROXY_URI,
+        stream_uri=RELAY_PROXY_URI,
+        events_uri=RELAY_PROXY_URI,
+    )
+
+    return LDClient(config)
+
+
+def main():
+    client = get_client()
+    context = Context.builder("user").set("key", "Sandy").build()
+
+    while True:
+        print("LDClient initialized:", client.is_initialized())
+        print("feature-flag-name: ", client.variation("abhay-test-unreliable", context, "default"))
+        time.sleep(1)
+
+if __name__ == "__main__":
+    main()
